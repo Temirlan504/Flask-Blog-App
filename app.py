3a8117e13026a911bd63360473803ccfc32bfb70
Temirlan504 <113613569+Temirlan504@users.noreply.github.com>
2024-06-30 20:27:47 -0400

    Add dummy data, and bootstrap
    Added dummy data to test website. Added bootstrap

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2020'
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
 
 if __name__ == '__main__':
